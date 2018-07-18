4ee15b792a4f6d688fc07cac10937a1a29b5ea00
Daylight <nik.dani1@googlemail.com>
2018-07-18 22:03:16 +0200

    commiting what i have so far

---
+++ real game/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour {
+
+    //Velocity
+    private float vUp;
+    private float vHor;
+    public float moveSpd = 5;
+	public float jmp = 8;
+	public float bulletSpd = 550;
+
+    private bool isGrounded = false;
+
+    //Other stuff
+    private Rigidbody2D rb2d;
+    Camera cam;
+
+    public GameObject bullet;
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+
+    // Use this for initialization
+    void Start () {
+
+        rb2d = GetComponent<Rigidbody2D>();
+        cam = Camera.main;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.15f, whatIsGround);
+
+        //Movement
+        if (Input.GetKey(KeyCode.A))
+
+        {
+
+            vHor = -moveSpd;
+
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+
+            vHor = moveSpd;
+
+        }
+        else
+        {
+
+            vHor = 0f;
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+
+        {
+			if (isGrounded)
+            vUp = jmp;
+
+        }
+        else
+        {
+
+            vUp = 0;
+
+        }
+
+        //use this code for aimed bullets
+        ///*
+        if (Input.GetMouseButtonDown(0))
+
+        {
+            if (GameObject.FindGameObjectsWithTag("bullet").Length < 3)
+
+            { 
+                
+                Vector2 bulletDirection = new Vector2(Input.mousePosition.x - cam.WorldToScreenPoint(transform.position).x, Input.mousePosition.y - cam.WorldToScreenPoint(transform.position).y);
+                bulletDirection.Normalize();
+                GameObject b = (GameObject)(Instantiate(bullet, transform.position, Quaternion.identity));
+                b.GetComponent<Rigidbody2D>().AddForce(bulletDirection * bulletSpd);
+
+                
+            }
+        }
+        //*/
+
+        //Use this code for cardinal direction bullets
+        /*
+        if(Input.GetKeyDown(KeyCode.LeftArrow) && GameObject.FindGameObjectsWithTag("bullet").Length < 3)
+          
+        {
+          
+             GameObject b = (GameObject)(Instantiate(bullet, transform.position, Quaternion.identity));
+             b.GetComponent<Rigidbody2D>().AddForce(new Vector2(-bulletSpd,0));
+         
+        }
+         
+        if(Input.GetKeyDown(KeyCode.RightArrow) && GameObject.FindGameObjectsWithTag("bullet").Length < 3)
+         
+        {
+        
+             GameObject b = (GameObject)(Instantiate(bullet, transform.position, Quaternion.identity));
+             b.GetComponent<Rigidbody2D>().AddForce(new Vector2(bulletSpd,0));
+        
+        }
+          
+        if(Input.GetKeyDown(KeyCode.UpArrow) && GameObject.FindGameObjectsWithTag("bullet").Length < 3)
+          
+        {
+          
+            GameObject b = (GameObject)(Instantiate(bullet, transform.position, Quaternion.identity));
+            b.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,bulletSpd));
+         
+        }
+          
+        if(Input.GetKeyDown(KeyCode.DownArrow) && GameObject.FindGameObjectsWithTag("bullet").Length < 3)
+         
+        {
+          
+            GameObject b = (GameObject)(Instantiate(bullet, transform.position, Quaternion.identity));
+            b.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,-bulletSpd));
+          
+        }
+        */
+        
+    }
+
+    void FixedUpdate () {
+
+        rb2d.velocity = new Vector2(vHor, rb2d.velocity.y + vUp);
+
+    }
+}
---
+++ real game/Assets/Scripts/script_Bullet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_Bullet : MonoBehaviour {
+
+    Camera cam;
+    Vector2 pos; 
+
+    // Use this for initialization
+    void Start () {
+
+        cam = Camera.main;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        pos = new Vector2(cam.WorldToScreenPoint(transform.position).x, cam.WorldToScreenPoint(transform.position).y);
+
+
+        if (pos.x<0 || pos.x>Screen.width || pos.y<0 || pos.y>Screen.height)
+        {
+            Destroy(gameObject);
+        }
+
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+
+		if (other.CompareTag ("Walls")) {
+			Destroy(gameObject);
+		}
+
+	}
+}
